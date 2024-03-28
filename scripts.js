d4396d0bb635cee32e4ea5c5695353adb3c4f878
Alex <schwimmeralexander@gmail.com>
2024-03-28 01:50:59 -0700

    Add book form will store inputs in library array, then append div with book object props in index container

--- scripts.js
+++ scripts.js
@@ -1,9 +1,43 @@
 const myLibrary = [];
+const addButton = document.getElementById("add-book");
+const addBookForm = document.getElementById("book-form");
+const title = document.getElementById("title");
+const author = document.getElementById("author");
+const pages= document.getElementById("pages");
+const read = document.getElementById("read");
+const container = document.getElementById("container");
 
-function Book() {
-  // the constructor...
+addButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    const newBook = new Book();
+    if(read.value === "on") {
+        read.value = "Yes";
+    } else {
+        read.value = "No";
+    }
+    newBook.title = title.value;
+    newBook.author = author.value;
+    newBook.pages = pages.value;
+    newBook.read = read.value;
+    addBookToLibrary(newBook);
+    console.log(newBook);
+    console.log(myLibrary);
+});
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
-function addBookToLibrary() {
-  // do stuff here
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+    const bookCard = document.createElement("div");
+    for(const prop in book) {  
+        const entry = document.createElement("div");
+        entry.innerHTML = `${prop}: ${book[prop]}`;
+        bookCard.append(entry);
+    }
+    container.append(bookCard);
 }
