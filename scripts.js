14d1c5e6093d2cd610560090f0db2b21c14fe6b9
Alex <schwimmeralexander@gmail.com>
2024-03-28 18:41:21 -0700

    Add labels and complete styling for container and book cards

--- scripts.js
+++ scripts.js
@@ -24,10 +24,10 @@ addButton.addEventListener("click", (event) => {
         read.value = "No";
     }
     
-    newBook.title = title.value;
-    newBook.author = author.value;
-    newBook.pages = pages.value;
-    newBook.read = read.value;
+    newBook.Title = title.value;
+    newBook.Author = author.value;
+    newBook.Pages = pages.value;
+    newBook.Read = read.value;
     addBookToLibrary(newBook);
     displayBooks();
     addBookForm.reset();
@@ -35,10 +35,10 @@ addButton.addEventListener("click", (event) => {
 });
 
 function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+  this.Title = title;
+  this.Author = author;
+  this.Pages = pages;
+  this.Read = read;
 }
 
 function addBookToLibrary(book) {
@@ -50,7 +50,7 @@ function displayBooks() {
         const bookCard = document.createElement("div");
         for(const prop in book) {
             const entry = document.createElement("div");
-            entry.innerHTML = `${prop}: ${book[prop]}`;
+            entry.innerHTML = `<strong>${prop}</strong>: ${book[prop]}`;
             bookCard.append(entry);
         }
         const removeBook = document.createElement("button");
@@ -64,10 +64,10 @@ function displayBooks() {
         markRead.innerHTML = "Mark Read";
         markRead.addEventListener("click", () => {
             const index = myLibrary.indexOf(book);
-            if (myLibrary[index].read === "Yes") {
-                myLibrary[index].read = "No";
+            if (myLibrary[index].Read === "Yes") {
+                myLibrary[index].Read = "No";
             } else {
-                myLibrary[index].read = "Yes";
+                myLibrary[index].Read = "Yes";
             }
             displayBooks();
         })
